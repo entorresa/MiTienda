b41bb559dfc2e3c451abd708c9bc65a8f91bbc41
ddiaz <ddiaz@dte.com>
2026-05-18 18:09:39 -0400

    agregado init . closed #19 .

--- models/request_api_mitienda.py
+++ models/request_api_mitienda.py
@@ -1,6 +1,7 @@
 from odoo import api, models, fields
 from odoo.exceptions import ValidationError
-
+import logging
+_logger = logging.getLogger(__name__)
 
 class RequestApiMiTienda(models.AbstractModel):
     _name = "request.api.mitienda"
@@ -13,4 +14,11 @@ class RequestApiMiTienda(models.AbstractModel):
     cabecera = fields.Json(string="Cabecera", default=lambda self: {'Content-Type': 'application/json'})
 
     def init(self, company_id = None):
-        
+        self.company_id = company_id or self.env.company
+        conexion_id = self.env['mitienda.pe.conexion'].search([('company_id','=', self.company_id),('activo','=', True)],limit=1)
+        self.conexion_id = conexion_id
+        if conexion_id:
+            self.cabecera.update({'Authorization': f"Bearer {conexion_id.token}"} if conexion_id.token_header == 'Bearer' else {'token': conexion_id.token})
+            return True
+        else:
+            _logger.error("No existe  una conexión activa para la compania seleccionada: %s", self.company_id.name)
