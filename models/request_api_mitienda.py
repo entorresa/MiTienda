264cd552a343230150ec67574aa29324c23751a2
ddiaz <ddiaz@dte.com>
2026-05-18 18:36:54 -0400

    adicionado metodo probar_conexion . closed #20 .

--- models/request_api_mitienda.py
+++ models/request_api_mitienda.py
@@ -1,5 +1,6 @@
 from odoo import api, models, fields
 from odoo.exceptions import ValidationError
+import requests
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -15,10 +16,50 @@ class RequestApiMiTienda(models.AbstractModel):
 
     def init(self, company_id = None):
         self.company_id = company_id or self.env.company
-        conexion_id = self.env['mitienda.pe.conexion'].search([('company_id','=', self.company_id),('activo','=', True)],limit=1)
+        conexion_id = self.env['mitienda.pe.conexion'].search([('company_id','=', self.company_id.id),('activo','=', True)],limit=1)
         self.conexion_id = conexion_id
         if conexion_id:
             self.cabecera.update({'Authorization': f"Bearer {conexion_id.token}"} if conexion_id.token_header == 'Bearer' else {'token': conexion_id.token})
             return True
         else:
             _logger.error("No existe  una conexión activa para la compania seleccionada: %s", self.company_id.name)
+
+    def request_api(self, endpoint, notificacion=False, mensaje='Éxito'):
+        error = False
+        data = {
+            'success': False,
+            'error': {
+                'code': -1,
+                'message': '',
+            },
+            'data': [],
+            'meta': {},
+        }
+        try:
+            if self.conexion_id.token_expiracion > fields.Datetime.now() or not self.conexion_id:
+                data['error']['message'] = 'Token API expirado' if self.conexion_id.token_expiracion > fields.Datetime.now() else 'No se encontró una conexión activa'
+            else:
+                respuesta = requests.get(self.conexion_id.url + endpoint, headers=self.cabecera, timeout=self.conexion_id.timeout_api)
+                respuesta.raise_for_status()
+                data = respuesta.json()
+        except Exception as e:
+            data['error']['message'] = str(e)
+        finally:
+            if notificacion:
+                return {
+                    'type': 'ir.actions.client',
+                    'tag': 'display_notification',
+                    'params': {
+                        'title': 'Éxito' if data.get('success') else 'Error',
+                        'message': mensaje if data.get('success') else data.get('error', {}).get('message'),
+                        'type': 'success' if data.get('success') else 'error',
+                        'sticky': False,
+                    },
+                }
+            else:
+                return data
+
+
+    def probar_conexion(self):
+        endpoint = '/mitienda/paymentlinks'
+        return self.request_api(endpoint, True, 'Conexión establecida')
