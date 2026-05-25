4cb5130342b559a57dc940ac226631f22f0f1bd2
ddiaz <ddiaz@dte.com>
2026-05-25 17:47:29 -0400

    adicionado buscar ventas v2

--- services/request_api_mitienda.py
+++ services/request_api_mitienda.py
@@ -1,7 +1,7 @@
 from odoo import fields
 import requests
 import logging
-
+from pprint import pp
 _logger = logging.getLogger(__name__)
 
 class RequestApiMiTienda:
@@ -67,3 +67,11 @@ class RequestApiMiTienda:
         fecha_fin = fields.Date.to_string(fecha_fin)
         endpoint = f"/mitienda/orders/codes/{fecha_inicio}/{fecha_fin}"
         return self.request_api(endpoint)
+
+    def buscar_ventas_v2(self, fecha_inicio=fields.Date.today(), fecha_fin=fields.Date.today(),  pagina=1):
+        fecha_inicio = fields.Date.to_string(fecha_inicio)
+        fecha_fin = fields.Date.to_string(fecha_fin)
+        endpoint = f'/mitienda/orders?status=1&order=date_created&otype=asc&from={fecha_inicio}&to={fecha_fin}&page={pagina}'
+        res = self.request_api(endpoint)
+        pp(res)
+        return res
