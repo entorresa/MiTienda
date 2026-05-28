59d7a6c4f5010b5ca454bcc1206818546873eba0
ddiaz <ddiaz@dte.com>
2026-05-28 18:18:57 -0400

    commit intermedio

--- models/mitienda_pe_product.py
+++ models/mitienda_pe_product.py
@@ -4,3 +4,11 @@ from odoo import api, models, fields
 class MiTiendaPeProduct(models.Model):
     _name = "mitienda.pe.product"
     _description = "Bitácora de sincronización de stock"
+    _rec_name = "fecha_sincronizacion desc"
+
+    conexion_id = fields.Many2one(string="Conexión", comodel_name="mitienda.pe.conexion", required=True)
+    product_template_id = fields.Many2one(string="Producto", comodel_name="product.template")
+    product_product_id = fields.Many2one(string="Variante", comodel_name="product.product")
+    fecha_sincronizacion = fields.Datetime(string="Fecha de sincronización", default=fields.Datetime.now)
+    company_id = fields.Many2one(string="Compañía", comodel_name="res.company", required=True, default=lambda self: self.env.company)
+    stock_picking_id = fields.Many2one(string="Operación de albarán", comodel_name="stock.picking")
