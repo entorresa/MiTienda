129d6a91aa1a2996b4f6d7c5b3213c95a39fca6d
ggutierrez <ggutierrez@dte.com.bo>
2026-06-23 09:27:00 -0400

    #123 Correccion del tablero dee sincronizaciones de ventas, con estilos

--- models/tablero.py
+++ models/tablero.py
@@ -49,7 +49,7 @@ class Tablero(models.Model):
             for venta in ultimas:
                 nombre = venta.sale_order_id.name if venta.sale_order_id else (venta.mitienda_order_code or '-') 
                 sale_id = venta.sale_order_id.id if venta.sale_order_id else 0
-                fecha = venta.fecha_sincronizacion.strftime('%d/%m/%Y %H:%M') if venta.fecha_sincronizacion else '-'
+                fecha = venta.fecha_sincronizacion.strftime('%d/%m/%Y') if venta.fecha_sincronizacion else '-'
                 filas.append(f'''
                     <div class="row">
                         <div class="col overflow-hidden text-start">
@@ -104,83 +104,3 @@ class Tablero(models.Model):
             'view_mode':'list,form',
             'target':'current',
         }
-
-    
-    
-'''
-    @api.model
-    def get_sync_data(self):
-        hoy = datetime.now().date()
-        hace_7_dias = hoy - timedelta(days=7)
-        ventas = self.env['mitienda.pe.sale.order'].search([
-            ('error', '=', False),
-            ('sale_order_id','!=',False),
-            ('mitienda_order_id','>',0),
-            ('mitienda_order_code','!=',''),
-            ('mitienda_order_status','=','1'),
-            ('fecha_sincronizacion','>=', hace_7_dias.strftime('%d/%m/%Y')),
-        ])
-        conteo_por_dia = {}
-        for i in range(7):
-            dia = hoy - timedelta(days=i)
-            conteo_por_dia[dia.strftime('%d/%m/%Y')] = 0
-        for venta in ventas:
-            dia = venta.fecha_sincronizacion.strftime('%d/%m/%Y')
-            if dia in conteo_por_dia:
-                conteo_por_dia[dia] += 1
-        fechas = sorted(conteo_por_dia.keys())
-        return {
-            'labels': fechas,
-            'data': [conteo_por_dia[f] for f in fechas],
-        }
-    @api.model
-    def get_ultimas_sincronizaciones(self):
-        ventas = self.env['mitienda.pe.sale.order'].search([
-            ('error', '=', False),
-            ('sale_order_id','!=',False),
-            ('mitienda_order_id','>',0),
-            ('mitienda_order_code','!=',''),
-            ('mitienda_order_status','=','1'),
-        ], limit=3, order='fecha_sincronizacion desc')
-
-        return[{
-            'id': v.id,
-            'nombre': str(v.fecha_sincronizacion),
-            'sale_order_id': v.sale_order_id.id if v.sale_order_id else False,
-            'sale_order_name': v.sale_order_id.name if v.sale_order_id else '',
-            'fecha':v.fecha_sincronizacion.strftime('%d/%m/%Y') if v.fecha_sincronizacion else '',
-        } for v in ventas]
-
-    @api.model
-    def set_favorite(self, is_favorite):
-        registro = self.search([('user_id','=',self.env.uid)], limit=1)
-        if registro:
-            registro.write({'is_favorite': is_favorite})
-        else:
-            self.create({
-                'user_id': self.env.uid,
-                'is_favorite': is_favorite,
-            })
-
-    @api.model
-    def set_color(self, color):
-        registro = self.search([('user_id','=',self.env.uid)], limit=1)
-        if registro:
-            registro.write({'color': color})
-        else:
-            self.create({
-                'user_id': self.env.uid,
-                'color': color,
-            })
-
-    @api.model
-    def get_preferencias(self):
-        registro = self.search([('user_id','=',self.env.uid)], limit=1)
-        if registro:
-            return{
-                'is_favorite': registro.is_favorite,
-                'color': registro.color,
-            }
-        return {'is_favorite': False, 'color':0}
-        
-'''
--- static/src/js/tablero.js
+++ static/src/js/tablero.js
@@ -1,9 +1,6 @@
 // /** @odoo-module **/
-// import { Component, useState, onMounted, useRef } from "@odoo/owl";
 import { registry } from "@web/core/registry";
-// import { useService } from "@web/core/utils/hooks";
 import { loadJS } from "@web/core/assets";
-// import { ControlPanel } from "@web/search/control_panel/control_panel";  
 
 const CHARTJS = "/web/static/lib/Chart/Chart.js";
 async function ensureChartJsLoaded(){
@@ -36,9 +33,9 @@ function renderChart(canvas){
                 {
                     label:"Sincronizaciones",
                     data,
-                    backgroundColor: "#714B67",
+                    backgroundColor: "#7ab9b9",
                     borderRadius: 4,
-                    maxBarThickness: 28,
+                    maxBarThickness: 45,
                 },
             ],
         },
@@ -120,131 +117,3 @@ registry.category("services").add("mitienda_pv_click_handler",{
         });
   },
 });
-
-
-// class TarjetaSincronizacion extends Component {
-//     static components = { ControlPanel }; 
-//     static template = "api_mitienda_peru.TarjetaSincronizacion";
-//     setup(){
-//         this.orm = useService("orm");
-//         this.action = useService("action");
-        
-
-//         this.state = useState({
-//             isFavorite: false,
-//             color: 0,
-//             sincronizaciones: [],
-//             chartData: {labels:[],data:[]},
-//             menuAbierto: false,
-//         });
-
-//         this.colors = [0,1,2,3,4,5,6,7,8,9,10,11];
-//         this.chartRef = useRef("chart");
-//         onMounted(async () => {
-//             await loadJS("/web/static/lib/Chart/Chart.js");
-//             await this.cargarDatos();
-//             this.inicializarGrafica();
-//         });
-//     }
-
-//     async cargarDatos() {
-//         const syncData = await this.orm.call(
-//             'mitienda.pe.tablero',
-//             'get_sync_data',
-//             []
-//         );
-//         const sincs = await this.orm.call(
-//             'mitienda.pe.tablero',
-//             'get_ultimas_sincronizaciones',
-//             []
-//         );
-//         const prefs = await this.orm.call(
-//             'mitienda.pe.tablero',
-//             'get_preferencias',
-//             []
-//         );
-
-//         this.state.chartData = syncData;
-//         this.state.sincronizaciones = sincs;
-//         this.state.isFavorite = prefs.is_favorite;
-//         this.state.color = prefs.color;
-//     }
-
-//     inicializarGrafica(){
-//         const ctx = this.chartRef.el.getContext('2d');
-//         new Chart(ctx,{
-//             type: 'bar',
-//             data: {
-//                 labels: this.state.chartData.labels,
-//                 datasets: [{
-//                     label: 'Sincronizaciones',
-//                     data: this.state.chartData.data,
-//                     backgroundColor: '#7C7BAD',
-//                     borderRadius: 4,
-//                 }]
-//             },
-//             options: {
-//                 responsive: true,
-//                 plugins:{
-//                     legend:{display: false },
-//                     tooltip:{
-//                         enabled: true,
-//                         callback:{
-//                             label: function(context){
-//                                 return ' ' + context.parsed.y + ' sincronizaciones';
-//                             } 
-//                         } 
-//                     }  
-//                 }, 
-//                 scales: {
-//                     y: {display: false,},
-//                     x: {grid:{display: false,}, ticks:{maxTicksLimit: 7}} 
-//                 }
-//             }
-//         });
-//     }
-
-//     toggleMenu(){
-//         this.state.menuAbierto = !this.state.menuAbierto;
-//     }
-//     async toggleFavorite(){
-//         this.state.isFavorite = !this.state.isFavorite;
-//         await this.orm.call(
-//             'mitienda.pe.tablero',
-//             'set_favorite',
-//             [this.state.isFavorite]
-//         );
-//         await this.cargarDatos();
-//         this.configurarControlPanel();
-//     }
-//     async setColor(color){
-//         this.state.color = color;
-//         this.state.menuAbierto = false;
-//         await this.orm.call(
-//             'mitienda.pe.tablero',
-//             'set_color',
-//             [color]
-//         );
-//     }
-//     verVenta(saleOrderId){
-//         if(!saleOrderId) return;
-//         this.action.doAction({
-//             type: 'ir.actions.act_window',
-//             res_model: 'sale.order',
-//             res_id: saleOrderId,
-//             views:[[false,'form']],
-//         });
-//     }
-//     verMas(){
-//         this.action.doAction({
-//             type: 'ir.actions.act_window',
-//             res_model: 'mitienda.pe.sale.order',
-//             views:[[false,'list']],
-//         });
-//     }
-// }
-// registry.category("actions").add(
-//     "mitienda_pe_tablero_view",
-//     TarjetaSincronizacion
-// );
-
