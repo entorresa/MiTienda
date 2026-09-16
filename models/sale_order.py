6d0d14aaf86d5a9e5808edd04b6d22a8da518c59
ggutierrez <ggutierrez@dte.com.bo>
2026-09-16 11:42:51 -0400

    closed #136 Se agregaron los atributos correspondientes, y el metodo computado del atributo mitienda_nro_factura_sunat que muestra los atributos primeros concatenados

--- models/sale_order.py
+++ models/sale_order.py
@@ -7,3 +7,12 @@ class SaleOrder(models.Model):
     mitienda_id = fields.Integer(string="ID")
     mitienda_code = fields.Char(string="Código")
     mitienda_sunat_pdf = fields.Char(string="Factura SUNAT")
+
+    mitienda_serie = fields.Char(string="Serie")
+    mitienda_correlative = fields.Char(string="Correlativo")
+    mitienda_nro_factura_sunat = fields.Char(string="Nro. Factura SUNAT", compute="_compute_mitienda_nro_factura_sunat")
+
+    @api.depends('mitienda_serie','mitienda_correlative')
+    def _compute_mitienda_nro_factura_sunat(self):
+        for record in self:
+            record.mitienda_nro_factura_sunat = f"{mitienda_serie} - {mitienda_correlative}"
