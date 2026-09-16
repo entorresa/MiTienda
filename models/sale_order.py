0f71b60b157593a52fe17595973b5c0d35e66cb4
Daniel Jiménez Jerez <djimenezjerez@gmail.com>
2026-09-16 14:19:23 -0400

    fixed #136

--- models/sale_order.py
+++ models/sale_order.py
@@ -7,18 +7,11 @@ class SaleOrder(models.Model):
     mitienda_id = fields.Integer(string="ID")
     mitienda_code = fields.Char(string="Código")
     mitienda_sunat_pdf = fields.Char(string="Factura SUNAT")
-
     mitienda_serie = fields.Char(string="Serie")
     mitienda_correlative = fields.Char(string="Correlativo")
-    mitienda_nro_factura_sunat = fields.Char(string="Nro. Factura SUNAT", compute="_compute_mitienda_nro_factura_sunat")
+    mitienda_nro_factura_sunat = fields.Char(string="Nro. Factura SUNAT", compute="_compute_mitienda_nro_factura_sunat", store=False)
 
     @api.depends('mitienda_serie','mitienda_correlative')
     def _compute_mitienda_nro_factura_sunat(self):
         for record in self:
-            parte1 = record.mitienda_serie or ""
-            parte2 = record.mitienda_correlative or ""
-
-            if parte1 and parte2:
-                record.mitienda_nro_factura_sunat = f"{parte1} - {parte2}"
-            else:
-                record.mitienda_nro_factura_sunat = parte1 or parte2 or ""
+            record.mitienda_nro_factura_sunat = '-'.join(x for x in [record.mitienda_serie, record.mitienda_correlative] if x)
