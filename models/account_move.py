3ddd88bbcf8058a2a181c07e6d8dbe785ed0c613
Daniel Jiménez Jerez <djimenezjerez@gmail.com>
2026-09-16 14:45:14 -0400

    closed #138

---
+++ models/account_move.py
@@ -0,0 +1,17 @@
+from odoo import api, models, fields
+
+
+class SaleOrder(models.Model):
+    _inherit = "account.move"
+
+    mitienda_id = fields.Integer(string="ID")
+    mitienda_code = fields.Char(string="Código")
+    mitienda_sunat_pdf = fields.Char(string="Factura SUNAT")
+    mitienda_serie = fields.Char(string="Serie")
+    mitienda_correlative = fields.Char(string="Correlativo")
+    mitienda_nro_factura_sunat = fields.Char(string="Nro. Factura SUNAT", compute="_compute_mitienda_nro_factura_sunat", store=False)
+
+    @api.depends('mitienda_serie','mitienda_correlative')
+    def _compute_mitienda_nro_factura_sunat(self):
+        for record in self:
+            record.mitienda_nro_factura_sunat = '-'.join(x for x in [record.mitienda_serie, record.mitienda_correlative] if x)
